5f12b446e67cdcfee28b12f21e5f97f76a305f2c
James Smith <codefiend@gmail.com>
2018-10-08 15:16:01 -0700

    Adding discord RPC integration. only updated when configuring plugin, as a placeholder

--- gen_discordrpc/gen_discordrpc.h
+++ gen_discordrpc/gen_discordrpc.h
@@ -6,6 +6,7 @@
 // plugin version (don't touch this)
 #define GPPHDR_VER 0x10
 
+static const char* APPLICATION_ID = "498742910875992064";
 #define PLUGIN_NAME "Discsord Rich Presence"
 
 #endif //gen_discordrpc_h
