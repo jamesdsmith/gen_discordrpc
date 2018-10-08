8a9573a37c287fa97da2cae339158f9e7ee48fa1
James Smith <codefiend@gmail.com>
2018-10-08 13:27:56 -0700

    Initial commit of plugin template code

---
+++ gen_discordrpc/gen_discordrpc.h
@@ -0,0 +1,11 @@
+#ifndef gen_discordrpc_h
+#define gen_discordrpc_h
+#include <windows.h>
+
+
+// plugin version (don't touch this)
+#define GPPHDR_VER 0x10
+
+#define PLUGIN_NAME "Discsord Rich Presence"
+
+#endif //gen_discordrpc_h
